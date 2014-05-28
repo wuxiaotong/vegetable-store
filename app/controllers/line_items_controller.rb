93c0eed0cb2eb195294de89f68e6af16253e46d6
wuxiaotong <wuxiaotongjy@gmail.com>
2014-05-27 18:20:58 -0700

    vegetable

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,8 +30,9 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @line_item }
+        format.html { redirect_to store }
+        format.js   { @current_item = @line_item}      
+        format.json { render json: @line_item, status:  :created, location: @line_item }
       else
         format.html { render action: 'new' }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,7 @@ class LineItem < ActiveRecord::Base
   belongs_to :cart
   attr_accessible :cart_id, :product_id, :quantity
   attr_accessible :product
+  def total_price
+  	product.price * quantity
+  end
 end
